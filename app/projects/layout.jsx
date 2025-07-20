4aa3340dddd73006497fe40ee4836ea15f69a917
kevinalvarel <alvarelkevin12@gmail.com>
2025-07-20 12:14:25 +0700

    Remove Header component from RootLayout

--- app/projects/layout.jsx
+++ app/projects/layout.jsx
@@ -23,7 +23,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${poppins.variable} ${jost.variable} antialiased`}>
-        <Header />
         {children}
       </body>
     </html>
