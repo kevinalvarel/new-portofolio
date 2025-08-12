7f671da9039b2354eaba0e3df2812d78a86c228c
kevinalvarel <alvarelkevin12@gmail.com>
2025-08-12 19:36:20 +0700

    Add Work and CanvasRevealEffect components; update Page to include Work component and add new dependencies for Three.js and React Three Fiber

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { HeroProject } from "./components/Hero";
 import Highlight from "./components/Highlight";
+import { Work } from "./components/Work";
 
 const Page = () => {
   return (
     <>
       <HeroProject />
       <Highlight />
+      <Work />
     </>
   );
 };
