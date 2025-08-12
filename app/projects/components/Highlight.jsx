af5b962ea22c08c737b4d8e962db2c817f44b850
kevinalvarel <alvarelkevin12@gmail.com>
2025-08-12 12:23:19 +0700

    Refactor Highlight component; add project description for Resumize AI and improve layout with new heading and styling.

--- app/projects/components/Highlight.jsx
+++ app/projects/components/Highlight.jsx
@@ -7,82 +7,91 @@ import Resumize2 from "@/public/images/Resumize2.png";
 
 export default function Highlight() {
   return (
-    <div className="relative w-screen mx-auto container gap-4 px-10 grid grid-cols-1 md:grid-cols-2 mb-10">
-      <div className="flex justify-center items-start flex-col mb-5 ">
-        <div className="images relative w-full  aspect-square">
-          <div className="absolute top-28 left-10 h-[40%]  aspect-video grayscale hover:grayscale-0 transition-all ease duration-300 hover:scale-150 z-10">
-            <motion.div
-              initial={{ opacity: 0, scale: 0.5, x: 100 }}
-              whileInView={{
-                opacity: 1,
-                scale: 1,
-                x: 0,
-              }}
-              className="w-full h-full shadow-lg"
-            >
-              <Image
-                src={Resumize1}
-                alt="Resumize Project"
-                layout="fill"
-                objectFit="cover"
-                placeholder="blur"
-                className="rat"
-              />
-            </motion.div>
-          </div>
-          <div className="absolute top-8 right-28 h-[30%]  aspect-video grayscale hover:grayscale-0 transition-all ease duration-300 hover:scale-150">
-            <motion.div
-              initial={{
-                opacity: 0,
-                scale: 0.5,
-                x: -100,
-              }}
-              whileInView={{
-                opacity: 1,
-                scale: 1,
-                x: 0,
-              }}
-              transition={{ delay: 0.3 }}
-              className="w-full h-full shadow-lg "
-            >
-              <Image
-                src={Resumize2}
-                alt="Resumize Project"
-                layout="fill"
-                objectFit="cover"
-                placeholder="blur"
-                objectPosition="0% 0%"
-              />
-            </motion.div>
-          </div>
+    <>
+      <div className="mt-10 flex flex-col justify-start items-center w-full pl-10 md:pl-32">
+        <div className="flex justify-center items-center flex-col my-5 self-start ">
+          <h1 className="text-3xl font-bold mt-3">On Progress</h1>
         </div>
       </div>
-      <motion.div
-        className="flex justify-center items-start flex-col mb-5 md:px-10"
-        initial={{
-          opacity: 0,
-          x: 200,
-        }}
-        whileInView={{
-          opacity: 1,
-          x: 0,
-        }}
-        transition={{
-          delay: 0.5,
-          type: "spring",
-        }}
-      >
-        <h2 className="text-2xl font-bold tracking-wider mb-3">Resumize AI</h2>
-        <p className="text-gray-600 text-justify title text-lg">
-          Resumize AI is an AI-powered project designed to help users improve
-          the quality of their resumes or CVs. Built with Next.js, TypeScript,
-          Tailwind CSS, shadcn/ui, and PuterJS, the application allows users to
-          upload or fill in their resume details directly on the platform. The
-          AI system analyzes the content of the resume, providing a score based
-          on factors such as structure, grammar, readability, and relevance to
-          the desired job position.
-        </p>{" "}
-      </motion.div>
-    </div>
+      <div className="relative w-screen mx-auto container gap-4 px-10 grid grid-cols-1 md:grid-cols-2 mb-10">
+        <div className="flex justify-center items-start flex-col mb-5 ">
+          <div className="images relative w-full  aspect-square">
+            <div className="absolute top-28 left-10 h-[40%]  aspect-video grayscale hover:grayscale-0 transition-all ease duration-300 hover:scale-150 z-10">
+              <motion.div
+                initial={{ opacity: 0, scale: 0.5, x: 100 }}
+                whileInView={{
+                  opacity: 1,
+                  scale: 1,
+                  x: 0,
+                }}
+                className="w-full h-full shadow-lg"
+              >
+                <Image
+                  src={Resumize1}
+                  alt="Resumize Project"
+                  layout="fill"
+                  objectFit="cover"
+                  placeholder="blur"
+                  className="rat"
+                />
+              </motion.div>
+            </div>
+            <div className="absolute top-8 right-28 h-[30%]  aspect-video grayscale hover:grayscale-0 transition-all ease duration-300 hover:scale-150">
+              <motion.div
+                initial={{
+                  opacity: 0,
+                  scale: 0.5,
+                  x: -100,
+                }}
+                whileInView={{
+                  opacity: 1,
+                  scale: 1,
+                  x: 0,
+                }}
+                transition={{ delay: 0.3 }}
+                className="w-full h-full shadow-lg "
+              >
+                <Image
+                  src={Resumize2}
+                  alt="Resumize Project"
+                  layout="fill"
+                  objectFit="cover"
+                  placeholder="blur"
+                  objectPosition="0% 0%"
+                />
+              </motion.div>
+            </div>
+          </div>
+        </div>
+        <motion.div
+          className="flex justify-center items-start flex-col mb-5 md:px-10"
+          initial={{
+            opacity: 0,
+            x: 200,
+          }}
+          whileInView={{
+            opacity: 1,
+            x: 0,
+          }}
+          transition={{
+            delay: 0.5,
+            type: "spring",
+          }}
+        >
+          <h2 className="text-2xl font-bold tracking-wider mb-3">
+            Resumize AI
+          </h2>
+          <p className="text-gray-600 text-justify title text-lg">
+            Resumize AI is an AI-powered project designed to help users improve
+            the quality of their resumes or CVs. Built with Next.js, TypeScript,
+            Tailwind CSS, and PuterJS, the application allows users to upload or
+            fill in their resume details directly on the platform. The AI system
+            analyzes the content of the resume, providing a score based on
+            factors such as structure, grammar, readability, and relevance to
+            the desired job position.
+          </p>{" "}
+        </motion.div>
+      </div>
+    </>
   );
 }
