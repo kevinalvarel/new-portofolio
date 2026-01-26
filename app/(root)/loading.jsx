b3cd80a6ee660ea6cb13727713cdb77d4eb3dfff
kevinalvarel <alvarelkevin12@gmail.com>
2026-01-26 08:37:09 +0700

    chore: add shadcn dependency to package.json

--- app/(root)/loading.jsx
+++ app/(root)/loading.jsx
@@ -3,7 +3,7 @@ import { LoaderOne } from "@/components/Loader";
 export default function Loading() {
   return (
     <div className="flex items-center justify-center h-screen">
-      <LoaderOne />;
+      <LoaderOne />
     </div>
   );
 }
